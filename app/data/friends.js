431d79f367faad3b103a8a95a49e244c6ce2f39c
unknown <caylenepicache@gmail.com>
2018-04-26 21:58:55 -0700

    update style

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,25 +1,9 @@
 // FRIENDS (DATA)
 // =============================================================
 var friends = [
-    {
-    name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
   {
-      name: "Caylene",
-      photo: "link here",
+      name: "Bucky",
+      photo: "../public/images/IMG_4510.jpg",
       scores: [
           5,
           5,
