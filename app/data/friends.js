5077bfc282dde1fb888054b61ed15052223cd707
unknown <caylenepicache@gmail.com>
2018-04-26 22:43:13 -0700

    adding friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,7 +17,43 @@ var friends = [
           1
       ]
 
-  }
+  },
+  {
+    name: "sPoNgEbOb SqUaRePaNtS",
+    photo: "https://pbs.twimg.com/media/DAmiYhhWsAAfGnm.jpg",
+    scores: [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+    ]
+
+},
+{
+    name: "Doodlebob",
+    photo: "https://ih1.redbubble.net/image.415472574.2975/ap,550x550,12x12,1,transparent,t.u2.png",
+    scores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+
+}
+
+
 ];
   
 
